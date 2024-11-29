bce99c3569bae693b032fb2a1de5a2adb64bf38b
walnashgit <avikragarwal@gmail.com>
2024-11-29 21:42:20 +0530

    updated dataset to download

--- train.py
+++ train.py
@@ -80,8 +80,8 @@ def train():
         transforms.Normalize((0.1307,), (0.3081,))
     ])
     
-    train_dataset = datasets.MNIST('../data', train=True, download=False, transform=transform)
-    test_dataset = datasets.MNIST('../data', train=False, download=False, transform=transform)
+    train_dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('../data', train=False, download=True, transform=transform)
     
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False)
